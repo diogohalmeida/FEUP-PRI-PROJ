2723fdbafc37694f1dd18cdd60b1433d38773150
Diogo Almeida <diogo@pop-os.localdomain>
2021-12-02 21:04:43 +0000

    Schema added to separate by comma
    Former-commit-id: 14632cd0c567a8e355deaaa30bf0331a30f19046

--- scripts/pipeline/refine.py
+++ scripts/pipeline/refine.py
@@ -57,26 +57,32 @@ print("After removing books that are not in review dataset: " + str(len(dataset)
 #Fix ISBN (make it a string)
 dataset["isbn"] = dataset["isbn"].map(str)
 
+# genre_and_votes_list = []
+# for i in dataset["genre_and_votes"].tolist():
+#     genre_and_votes_list.append(i.split(","))
 
-# #Delete books from recommended and series cells that were removed before
-to_delete = list()  # this will speed things up doing only 1 delete
-for id, row in dataset.iterrows():
-    if(not pandas.isnull(row.books_in_series)):
-        series_ids = [int(s) for s in str(row.books_in_series).split(',')]
-        for serie_id in series_ids:
-            if(serie_id not in ids):
-                series_ids.remove(serie_id)
-        row["books_in_series"] = series_ids
+# dataset["genre_and_votes"] = genre_and_votes_list
+
+
+# # #Delete books from recommended and series cells that were removed before
+# to_delete = list()  # this will speed things up doing only 1 delete
+# for id, row in dataset.iterrows():
+#     if(not pandas.isnull(row.books_in_series)):
+#         series_ids = [int(s) for s in str(row.books_in_series).split(',')]
+#         for serie_id in series_ids:
+#             if(serie_id not in ids):
+#                 series_ids.remove(serie_id)
+#         row["books_in_series"] = series_ids
             
 
-#to_delete = list()  # this will speed things up doing only 1 delete
-for id, row in dataset.iterrows():
-    if(not pandas.isnull(row.recommended_books)):
-        series_ids = [int(s) for s in str(row.recommended_books).split(',')]
-        for serie_id in series_ids:
-              if(serie_id not in ids):
-                  series_ids.remove(serie_id)
-        row["recommended_books"] = series_ids
+# #to_delete = list()  # this will speed things up doing only 1 delete
+# for id, row in dataset.iterrows():
+#     if(not pandas.isnull(row.recommended_books)):
+#         series_ids = [int(s) for s in str(row.recommended_books).split(',')]
+#         for serie_id in series_ids:
+#               if(serie_id not in ids):
+#                   series_ids.remove(serie_id)
+#         row["recommended_books"] = series_ids
 
 
-dataset.to_csv('../../dataset/goodreads_books_clean.csv', index = False)
+dataset.to_json('../../dataset/goodreads_books_clean.json', orient="records")
