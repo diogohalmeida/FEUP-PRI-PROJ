7e600dfce7ce5c4b1f22983ecad16bc74e243e5d
Diogo Almeida <diogohenriquealmeida@hotmail.com>
2021-12-23 17:48:40 +0000

    Added first version of GUI
    Former-commit-id: c808ff778fa81677d795011f0abac61d660b2560

---
+++ webapp/routes.js
@@ -0,0 +1,48 @@
+const SolrNode = require("solr-node");
+const express = require("express");
+
+var client = new SolrNode({
+    host: "127.0.0.1",
+    port: "8983",
+    core: "books",
+    protocol: "http"
+})
+
+
+const router = express.Router();
+
+router.get("/", function(req, res) {
+    console.log("Basic web page")
+    res.render("index");
+})
+
+router.get("/search", (req,res) => {
+    const search = req.query["query"];
+
+    const searchQuery = client.query()
+    .qop("OR")
+    .q(search)
+    .addParams({
+        wt:"json",
+        indent: true,
+    }).edismax()
+    .mltQuery("qf=" + ["title%5E5","author%5E3","series%5E3","description%5E1"].join("%20"))
+
+    client.search(searchQuery, function(err, result){
+        if (err) {
+            console.log(err)
+            return
+        }
+        const response = result.response
+
+        res.render("index", {data: {
+            userQuery: search,
+            books: response.docs
+            }
+        })
+    })
+
+})
+
+
+module.exports = router
