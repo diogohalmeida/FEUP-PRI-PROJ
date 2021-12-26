22cfbc2bea3b0befcaf686d1c1cb839e3e81e961
Diogo Almeida <diogohenriquealmeida@hotmail.com>
2021-12-26 22:21:58 +0000

    More progress on the GUI
    Former-commit-id: e1ab046dc9baa5cc74a86de51fab158bf15402cf

--- webapp/app.js
+++ webapp/app.js
@@ -8,7 +8,9 @@ app.set("port", 3000)
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.engine('ejs', require('ejs').__express);
+app.use(express.static(__dirname));
 app.use(routes)
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000")
