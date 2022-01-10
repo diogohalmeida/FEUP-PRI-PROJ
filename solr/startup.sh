5a1c124d86d82144dc7a115e2a64bafbc7ff4287
Diogo Almeida <diogohenriquealmeida@hotmail.com>
2022-01-10 17:56:33 +0000

    Started openNLP implementation
    Former-commit-id: 2bc952b9afe837822cc2d57803d868fee35e13cb

--- solr/startup.sh
+++ solr/startup.sh
@@ -5,11 +5,35 @@ precreate-core reviews
 precreate-core books
 
 # Start Solr in background mode so we can use the API to upload the schema
-solr start
+solr start "-Denable.runtime.lib=true"
 
 sleep 10
 
+mkdir /var/solr/data/books/conf
+
+mkdir /var/solr/data/books/lib
+
 cp /data/synonyms.txt /var/solr/data/books/conf
+
+cp /models/en-ner-person.bin /var/solr/data/books/conf/en-ner-person.bin
+
+cp /models/en-sent.bin /var/solr/data/books/conf/en-sent.bin
+
+cp /models/en-token.bin /var/solr/data/books/conf/en-token.bin
+
+cp contrib/analysis-extras/lib/icu4j-62.1.jar /var/solr/data/books/lib
+cp contrib/analysis-extras/lib/opennlp-tools-1.9.2.jar /var/solr/data/books/lib
+cp contrib/analysis-extras/lib/morfologik-stemming-2.1.5.jar /var/solr/data/books/lib
+cp contrib/analysis-extras/lib/morfologik-fsa-2.1.5.jar /var/solr/data/books/lib
+cp contrib/analysis-extras/lib/morfologik-polish-2.1.5.jar /var/solr/data/books/lib
+cp contrib/analysis-extras/lib/morfologik-ukrainian-search-4.9.1.jar /var/solr/data/books/lib
+
+cp contrib/analysis-extras/lucene-libs/lucene-analyzers-icu-8.10.1.jar /var/solr/data/books/lib
+cp contrib/analysis-extras/lucene-libs/lucene-analyzers-morfologik-8.10.1.jar /var/solr/data/books/lib
+cp contrib/analysis-extras/lucene-libs/lucene-analyzers-opennlp-8.10.1.jar /var/solr/data/books/lib
+cp contrib/analysis-extras/lucene-libs/lucene-analyzers-smartcn-8.10.1.jar /var/solr/data/books/lib
+cp contrib/analysis-extras/lucene-libs/lucene-analyzers-stempel-8.10.1.jar /var/solr/data/books/lib
+
 
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
