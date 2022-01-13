69c1c6aa43334a3e37208619993ba001345e4175
Diogo Almeida <diogohenriquealmeida@hotmail.com>
2022-01-13 18:21:49 +0000

    Added Learn To Rank (LTR)
    Former-commit-id: d3db2acf7344ddf2ef70ccd580f441e3135feea2

--- solr/startup.sh
+++ solr/startup.sh
@@ -5,15 +5,11 @@ precreate-core reviews
 precreate-core books
 
 # Start Solr in background mode so we can use the API to upload the schema
-solr start
+sed -i $'/<\/config>/{e cat config.xml\n}' /var/solr/data/books/conf/solrconfig.xml
 
-sleep 10
-
-#mkdir /var/solr/data/books/conf
+solr start -Dsolr.ltr.enabled=true
 
 cp /data/synonyms.txt /var/solr/data/books/conf
-
-mkdir /var/solr/data/books/lib
 
 cp /models/en-ner-person.bin /var/solr/data/books/conf/en-ner-person.bin
 
@@ -25,24 +21,12 @@ cp /models/en-chunker.bin /var/solr/data/books/conf/en-chunker.bin
 
 cp /models/en-pos-maxent.bin /var/solr/data/books/conf/en-pos-maxent.bin
 
-cp contrib/analysis-extras/lib/icu4j-62.1.jar /var/solr/data/books/lib
-cp contrib/analysis-extras/lib/opennlp-tools-1.9.2.jar /var/solr/data/books/lib
-cp contrib/analysis-extras/lib/morfologik-stemming-2.1.5.jar /var/solr/data/books/lib
-#cp contrib/analysis-extras/lib/morfologik-fsa-2.1.5.jar /var/solr/data/books/lib
-#cp contrib/analysis-extras/lib/morfologik-polish-2.1.5.jar /var/solr/data/books/lib
-#cp contrib/analysis-extras/lib/morfologik-ukrainian-search-4.9.1.jar /var/solr/data/books/lib
-
-cp contrib/analysis-extras/lucene-libs/lucene-analyzers-icu-8.10.1.jar /var/solr/data/books/lib
-cp contrib/analysis-extras/lucene-libs/lucene-analyzers-morfologik-8.10.1.jar /var/solr/data/books/lib
-cp contrib/analysis-extras/lucene-libs/lucene-analyzers-opennlp-8.10.1.jar /var/solr/data/books/lib
-cp contrib/analysis-extras/lucene-libs/lucene-analyzers-smartcn-8.10.1.jar /var/solr/data/books/lib
-cp contrib/analysis-extras/lucene-libs/lucene-analyzers-stempel-8.10.1.jar /var/solr/data/books/lib
-
+sleep 15
 
 # Schema definition via API
-curl -X POST -H 'Content-type:application/json' \
-    --data-binary @/data/config.json \
-    http://localhost:8983/solr/books/config
+#curl -X POST -H 'Content-type:application/json' \
+#    --data-binary @/data/config.json \
+#    http://localhost:8983/solr/books/config
 
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @/data/reviews_schema.json \
@@ -52,6 +36,13 @@ curl -X POST -H 'Content-type:application/json' \
     --data-binary @/data/schema.json \
     http://localhost:8983/solr/books/schema
 	
+curl -XPUT -H 'Content-type:application/json' \
+    --data-binary @/ltr/my_efi_features.json \
+    http://localhost:8983/solr/books/schema/feature-store
+
+curl -XPUT -H 'Content-type:application/json' \
+    --data-binary @/ltr/my_efi_model.json \
+    http://localhost:8983/solr/books/schema/model-store
 
 # Populate collection
 bin/post -c reviews /data/goodreads_reviews_solr.csv
